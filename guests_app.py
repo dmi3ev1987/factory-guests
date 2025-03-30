44bded0d604c38f72c366ae378f8adf4710421a7
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-03-30 11:12:35 +0000

    Sqlite config for sql-alchemy.

--- guests_app.py
+++ guests_app.py
@@ -1,11 +1,13 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+db = SQLAlchemy(app)
 
 
 @app.route('/')
 def my_index_view():
-    print(app.config)
     return 'Это мой первый Flask-проект'
 
 
