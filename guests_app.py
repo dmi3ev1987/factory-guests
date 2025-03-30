592bb151ab7a2c9a17138cede7af3d13bc434874
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-03-30 12:38:15 +0000

    Add first model.

--- guests_app.py
+++ guests_app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,9 +6,25 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 db = SQLAlchemy(app)
 
 
+class Guest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    full_name = db.Column(db.String(128), nullable=False)
+    company_name = db.Column(db.String(128), nullable=False)
+    inviter = db.Column(db.String(128), nullable=False)
+    place_to_visit = db.Column(db.String(128), nullable=False)
+    time_start  = db.Column(db.DateTime, nullable=False)
+    time_end = db.Column(db.DateTime, nullable=False)
+    approved = db.Column(db.Boolean, default=False)
+
+
 @app.route('/')
 def my_index_view():
-    return 'Это мой первый Flask-проект'
+    quantity = Guest.query.count()
+    if quantity == 0:
+        return 'Нет гостей'
+    guests = Guest.query.all()
+    result = [{guest.id: guest.full_name} for guest in guests]
+    return jsonify(result)
 
 
 if __name__ == '__main__':
