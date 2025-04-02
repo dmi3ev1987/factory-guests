f2dfbc6d18e381d4f50b4d0baf204499ad446b4e
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-04-02 18:49:12 +0300

    Add Flask WTF.

--- guests_app.py
+++ guests_app.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_folder='static_dir')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SECRET_KEY'] = 'MY SECRET KEY'
+
 db = SQLAlchemy(app)
 
 
