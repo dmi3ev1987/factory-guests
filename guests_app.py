2436c9483e99ab96a86a660e6214170214e5ad59
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-04-02 16:49:32 +0300

    Add base html.

--- guests_app.py
+++ guests_app.py
@@ -1,7 +1,7 @@
 from flask import Flask, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static_dir')
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 db = SQLAlchemy(app)
 
