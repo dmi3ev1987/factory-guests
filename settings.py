7c045f2864720fb986036e3fdd5cc741a0a87f31
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-04-07 12:18:47 +0000

    Change env database_uri name.

--- settings.py
+++ settings.py
@@ -2,5 +2,5 @@ import os
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SECRET_KEY = os.getenv('SECRET_KEY')
