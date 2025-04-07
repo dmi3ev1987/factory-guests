a968c05c85a8ee03783ed8e8d87c6b40ea520a00
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-04-07 12:11:33 +0000

    Add view.

---
+++ guests_app/views.py
@@ -0,0 +1,50 @@
+from flask import render_template
+
+from . import app, db
+from .forms import GuestForm
+from .models import Guest
+
+
+@app.route('/')
+def index_view():
+    guests = Guest.query.with_entities(
+        Guest.full_name,
+        Guest.company_name,
+        Guest.inviter,
+        Guest.place_to_visit,
+        Guest.time_start,
+        Guest.time_end,
+        Guest.purpose,
+    ).all()
+    guests_list = [
+        {
+            'full_name': guest[0],
+            'company_name': guest[1],
+            'inviter': guest[2],
+            'place_to_visit': guest[3],
+            'time_start': guest[4],
+            'time_end': guest[5],
+            'purpose': guest[6],
+        }
+        for guest in guests
+    ]
+    return render_template('index.html', guests=guests_list)
+
+
+@app.route('/request-form', methods=['GET', 'POST'])
+def request_form_view():
+    form = GuestForm()
+    if form.validate_on_submit():
+        guest = Guest(
+            full_name=form.full_name.data,
+            company_name=form.company_name.data,
+            inviter=form.inviter.data,
+            place_to_visit=form.place_to_visit.data,
+            time_start=form.time_start.data,
+            time_end=form.time_end.data,
+            purpose=form.purpose.data,
+        )
+        db.session.add(guest)
+        db.session.commit()
+        return 'Заявка отправлена'
+    return render_template('request_form.html', form=form)
