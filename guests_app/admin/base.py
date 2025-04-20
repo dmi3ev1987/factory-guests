d222bdbe02d4b1b88408cc05d0aa330085d35b20
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-04-20 12:26:08 +0000

    Change access to admin panel.

--- guests_app/admin/base.py
+++ guests_app/admin/base.py
@@ -5,7 +5,7 @@ from flask_login import current_user
 
 class MyAdminIndexView(AdminIndexView):
     def is_accessible(self):
-        return current_user.is_authenticated
+        return current_user.is_authenticated and current_user.is_admin
 
     def inaccessible_callback(self, name, **kwargs):
         return redirect(url_for('login_view'))
