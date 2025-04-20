46920e30a35690fe4eac14dcfbcc9f517c5bf96c
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-04-20 10:22:54 +0000

    Add admin to env example.

--- settings.py
+++ settings.py
@@ -5,3 +5,6 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.getenv('SQLALCHEMY_DATABASE_URI')
     SECRET_KEY = os.getenv('SECRET_KEY')
     FLASK_ADMIN_SWATCH = os.getenv('FLASK_ADMIN_SWATCH')
+    ADMIN_USERNAME = os.getenv('ADMIN_USERNAME')
+    ADMIN_EMAIL = os.getenv('ADMIN_EMAIL')
+    ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD')
