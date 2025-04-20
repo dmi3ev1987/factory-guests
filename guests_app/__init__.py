e5ebcf739d0c1266e4929309b3329ec955a3492d
Andrey Dmitriev <42832639+dmi3ev1987@users.noreply.github.com>
2025-04-20 12:21:44 +0000

    Add cli command to create admin user.

--- guests_app/__init__.py
+++ guests_app/__init__.py
@@ -4,15 +4,32 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-from guests_app.admin import create_admin
+from guests_app.admin import create_admin_panel
 from settings import Config
 
-app = Flask(__name__, static_folder='static_dir')
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-login_manager = LoginManager(app)
-bcrypt = Bcrypt(app)
-admin = create_admin(app)
+db = SQLAlchemy()
+migrate = Migrate()
+bcrypt = Bcrypt()
+login_manager = LoginManager()
+
+
+def create_app():
+    app = Flask(__name__, static_folder='static_dir')
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+
+    from .cli_commands import init_cli_commands
+
+    init_cli_commands(app)
+
+    return app
+
+
+app = create_app()
+admin = create_admin_panel(app)
 
 from . import views  # noqa: F401
--- guests_app/admin/__init__.py
+++ guests_app/admin/__init__.py
@@ -1,26 +1,13 @@
 from flask_admin import Admin
 
-from guests_app import db
-from guests_app.admin.base import MyAdminIndexView
-from guests_app.models import User
+from .base import MyAdminIndexView
 
 
-def create_admin(app):
+def create_admin_panel(app):
+    """Создает панель администратора."""
     return Admin(
         app,
         name='Guests',
         template_mode='bootstrap3',
         index_view=MyAdminIndexView(),
     )
-
-
-def create_admin_user(app):
-    admin_email = app.config['ADMIN_EMAIL']
-    admin_user = User.query.filter_by(email=admin_email, is_admin=True).first()
-
-    if not admin_user:
-        admin_password = app.config['ADMIN_PASSWORD']
-        admin_user = User(email=admin_email, is_approver=True)
-        admin_user.set_password(admin_password)
-        db.session.add(admin_user)
-        db.session.commit()
