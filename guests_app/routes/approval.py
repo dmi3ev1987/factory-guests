b7ea46729af8548b21feee513f676c7312c7d106
Andrey Dmitriev <andrey.dmi3ev@gmail.com>
2025-04-27 02:47:10 +0300

    Approval filters.

--- guests_app/routes/approval.py
+++ guests_app/routes/approval.py
@@ -1,13 +1,14 @@
-from flask import render_template
+from flask import redirect, render_template, url_for
 
-from guests_app import app
+from guests_app import app, db
+from guests_app.models import PassRequest
 from guests_app.routes.functions import get_guests
 
 
 @app.route('/approval')
 def approval_view():
     """Страница одобрения заявок на пропуска."""
-    guests = get_guests()
+    guests = get_guests(approval_status='pending')
 
     guests_list = [dict(guest._asdict()) for guest in guests]
 
@@ -16,11 +17,13 @@ def approval_view():
 
 @app.route('/approve_request/<request_id>', methods=['POST'])
 def approve_request(request_id):
-    # Your approval logic here
-    return request_id
+    pass_request = PassRequest.query.get(request_id)
+    pass_request.approved = True
+    db.session.commit()
+    return redirect(url_for('approval_view'))
 
 
 @app.route('/reject_request/<request_id>', methods=['POST'])
 def reject_request(request_id):
     # Your rejection logic here
-    return render_template('approval.html')
+    return redirect(url_for('approval_view'))
